cd455e91fb201a6a66e477285e70c84a007f617b
SebasGS-dev <sebastian.gonzalez@trespimedios.co>
2024-09-17 12:03:57 -0500

    reajuste de props para traerlos de Navigation

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,8 +1,6 @@
-import  { useState, useEffect } from "react";
 import EventForm from "./EventForm";
 import Calendar from "./Calendar";
 
-// Definir el tipo de evento
 interface Event {
     eventName: string;
     eventDate: string;
@@ -12,34 +10,14 @@ interface Event {
     id: number;
 }
 
-const MainApp = () => {
-    // Estado para manejar los eventos
-    const [events, setEvents] = useState<Event[]>([]);
-    const [id, setId] = useState<number>(0)
+interface MainAppProps {
+    addEvent: (newEvent: Event) => void
+    events: Event[];
+    deleteEvent: (event: Event) => void;
+    updateEventDate: (event: Event, newDate: string) => void;
+}
 
-    // Función para agregar un nuevo evento
-    const addEvent = (newEvent: Event) => {
-        const newId = id + 1;
-        setId(newId);
-        newEvent.id = newId;
-        const updatedEvents = [...events, newEvent];
-        setEvents(updatedEvents);
-    };
-
-     // Función para borrar un evento
-    const deleteEvent = (eventToDelete: Event) => {
-        const updatedEvents = events.filter((event) => event !== eventToDelete);
-        setEvents(updatedEvents);
-    };
-
-    // Función para actualizar la fecha de un evento
-    const updateEventDate = (updatedEvent: Event, newDate: string) => {
-        const updatedEvents = events.map((event) =>
-            event === updatedEvent ? { ...event, eventDate: newDate } : event
-        );
-        setEvents(updatedEvents);
-    };
-
+const MainApp = ({addEvent, events, deleteEvent, updateEventDate}: MainAppProps) => {
     return (
         <div className="App p-11">
             <h1 className="text-3xl font-bold text-center mb-10">
