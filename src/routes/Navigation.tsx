0c8452ccdf2479804d6c6f95d3c664492dbb8f48
SebasGS-dev <sebastian.gonzalez@trespimedios.co>
2024-09-17 10:35:22 -0500

    Ajustes logica Navigation

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,22 +1,66 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import MainApp from "../components/mainApp"
+import MainApp from "../components/MainApp"
 import EditEventPage from "../components/EditEventPage"
+import { useState } from "react";
 
+// Definir el tipo de evento
+interface Event {
+    eventName: string;
+    eventDate: string;
+    eventDetails: string;
+    user: string;
+    priority: string;
+    id: number;
+}
 
 const Navigation = () => {
+    // Estado para manejar los eventos
+    const [events, setEvents] = useState<Event[]>([]);
+    const [id, setId] = useState<number>(0)
+
+    // Función para agregar un nuevo evento
+    const addEvent = (newEvent: Event) => {
+        const newId = id + 1;
+        setId(newId);
+        newEvent.id = newId;
+        const updatedEvents = [...events, newEvent];
+        setEvents(updatedEvents);
+    };
+
+     // Función para borrar un evento
+    const deleteEvent = (eventToDelete: Event) => {
+        const updatedEvents = events.filter((event) => event !== eventToDelete);
+        setEvents(updatedEvents);
+    };
+
+    // Función para actualizar la fecha de un evento
+    const updateEventDate = (updatedEvent: Event, newDate: string) => {
+        const updatedEvents = events.map((event) =>
+            event === updatedEvent ? { ...event, eventDate: newDate } : event
+        );
+        setEvents(updatedEvents);
+    };
+
+    // Función para actualizar un evento
+    const updateEvent = (updatedEvent: Event) => {
+        const updatedEvents = events.map((event) =>
+            event.id === updatedEvent.id ? updatedEvent : event
+        );
+        setEvents(updatedEvents);
+    };
     
     return (
         <Router>
             <Routes>
                 <Route
                     path="/"
-                    element={<MainApp/>}
+                    element={<MainApp events={events} deleteEvent={deleteEvent} updateEventDate={updateEventDate} addEvent={addEvent}/>}
                 />
 
                 {/* Ruta para la página de edición */}
                 <Route
                     path="/edit/:id"
-                    element={<EditEventPage/>}
+                    element={<EditEventPage events={events} updateEvent={updateEvent}/>}
                 />
 
                 <Route path="*" element={<Navigate to="/" replace />} />
